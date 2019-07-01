69630297c46febbe620e6083b0d78aa7c7dc1c76
Yan <yan.efimov@gmail.com>
2019-07-01 23:42:54 +0300

    add spring boot

---
+++ alfaSpring/src/main/java/Runner.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.SpringApplication;
+
+public class Runner {
+    public static void main(String[] args) {
+        SpringApplication.run(Runner.class, args);
+    }
+}
