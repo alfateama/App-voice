e1a7ea2032b2697b9df1703dbec754a83f9ce312
Yan <yan.efimov@gmail.com>
2019-07-04 15:37:38 +0300

    add hibernate

--- alfaSpring/src/main/java/by/st/Runner.java
+++ alfaSpring/src/main/java/by/st/Runner.java
@@ -2,11 +2,13 @@ package by.st;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = "by.st")
 @RestController
+@EnableJpaAuditing
 public class Runner {
 
     @GetMapping("/")
